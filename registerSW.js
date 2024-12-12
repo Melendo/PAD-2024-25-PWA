f50456d91e7569b2df928641d7e24eec3131dac7
Melendo <nachomelendo@gmail.com>
2024-12-12 11:42:55 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/PAD-2024-25-PWA/sw.js', { scope: '/PAD-2024-25-PWA/' })})}
